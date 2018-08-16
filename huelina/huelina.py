4430847d4c0f33af6b04a173525ac218881400f6
DrNightingales <petersoloviev123@gmail.com>
2018-08-16 11:17:35 +0300

    Refactored code to object-oriented paradigm

--- huelina/huelina.py
+++ huelina/huelina.py
@@ -1,107 +1,121 @@
 # -*- coding: utf-8 -*- 
 # TODO: Unite encodings for windows and linux
-version = 0.3
-from huelina.question import Question
+__version__ = 0.3
+try:
+    from question import Question
+except:
+    from huelina.question import Question
 from colorama import Fore, init
 from random import shuffle
+from os import getcwd
+from pathlib import Path
 
+class quize():
+    def __init__(self):
+        self.questions = []
+        self.mistakes = set() 
+        
+        print(Fore.CYAN) # Color background text in blue
+        print(f"Добро пожаловать в Хуелина v{__version__}\n \
+        \nВаш каталог: {getcwd()}\n \
+        \nВведите имя файла с тестами (по умолчанию anat.txt) \
+        \nИспользуйте файлы anatN-ansi.txt на windows и anatN-utf8 на других OS")
 
-def load_questions(filepath):
-    """
-    Parses file defined by filepath
-    """
-    with open(filepath) as f:
-        for line in f.readlines():
-            if line[0].isnumeric():
-                questions.append(Question(line))
-            elif line[0] == "*":
-                questions[-1].add_correct(line[2:len(line) - 1])
-            elif line[0] == "#":
-                pass
+        file = Path(input())
+        print(file)
+        if file == '':
+            file = "data/anat.txt"
+        self.quize_file = file
+
+        self.load_questions()
+        self.start_and_end()
+        self.ask_questions()
+
+    def ask_questions(self):
+        chosen = self.questions[self.start:self.ends]
+        shuffle(chosen)
+        for q in chosen:
+            print(f"[{chosen.index(q) + 1}/{len(chosen)}]", end=" ")
+            q.printq()
+            no_mistakes = q.check(input())
+            if not no_mistakes:
+                self.mistakes.add(q)
+            print("")
+
+        while len(self.mistakes) != 0:
+
+            print(f"Количество Ваших ошибок: {len(self.mistakes)}\n"
+                "Сделать работу над ошибками? Введите 1 и нажмите Enter, чтобы выполнить "
+                "\n(Другие цифры или буквы, или просто нажатие Enter выключат программу)")
+            if input() == "1":
+                pending_rem = set()
+                i = 1
+                for q in self.mistakes:
+                    print(f"[{i}/{len(self.mistakes)}]", end=" ")
+                    q.printq()
+                    no_mistakes = q.check(input())
+                    if no_mistakes:
+                        pending_rem.add(q)
+                    i += 1
+                    print("")
+                self.mistakes = self.mistakes - pending_rem
             else:
-                questions[-1].add_incorrect(line[:len(line) - 1])
+                self.mistakes.clear()
+
+        
+    def load_questions(self):
+        """
+        Parses file defined by filepath
+        """
+        try:
+            with open(self.quize_file) as f:
+                for line in f.readlines():
+                    if line[0].isnumeric():
+                        self.questions.append(Question(line))
+                    elif line[0] == "*":
+                        self.questions[-1].add_correct(line[2:len(line) - 1])
+                    elif line[0] == "#":
+                        pass
+                    else:
+                        self.questions[-1].add_incorrect(line[:len(line) - 1])
+        except FileNotFoundError:
+            print(f"{Fore.RED}Файл не существует! Файл по умолчанию - anat.txt{Fore.CYAN}")
+            file="data/anat.txt"
+
+    def start_and_end(self):
+        #User specifies start and stop points
+        print("\nВведите номер вопроса, с которого хотите начать (по умолчанию 1)")
+        self.start = input()
+
+        try:
+            if self.start == '':
+                self.start = 0
+            else:
+                self.start = int(self.start) - 1
+        except ValueError:
+            print(f"{Fore.RED}Неверный ввод, вы начнете с первого вопроса{Fore.CYAN}")
+            self.start = 0
+
+        print(f"Введите номер вопроса, которым хотите закончить (по умолчанию {len(self.questions)})")
+        self.ends = input()
+
+        try:
+            if self.ends == '':
+                self.ends = len(self.questions)
+            elif int(self.ends) > len(self.questions) or int(self.ends) < self.start:
+                print(f"{Fore.RED}Неверный ввод, вы закончите вопросом номер {len(self.questions)} {Fore.CYAN}")
+                self.ends = len(self.questions)
+            else:
+                self.ends = int(self.ends)
+        except ValueError:
+            print(f"{Fore.RED}Неверный ввод, вы закончите вопросом номер {len(self.questions)} {Fore.CYAN}")
+            self.ends = len(self.questions)
+
+        print(f"Вопросы в выбранном Вами диапазоне перемешаны автоматически \
+                    \n{Fore.RED}Вводите ответ как на ЕГЭ \
+                    \n(цифры в любой последовательности, без дополнительных знаков){Fore.CYAN}\n")
 
 
 if __name__ == "__main__":
     init()
-    #print(Style.BRIGHT) # Set style 
-    print(Fore.CYAN) # Color background text in blue
-
-    print(f"Добро пожаловать в Хуелина v{version}\n \
-        \nВведите имя файла с тестами (по умолчанию anat.txt) \
-        \nИспользуйте файлы anatN-ansi.txt на windows и anatN-utf8 на других OS")
-
-    file = input()
-
-    if file == '':
-        file = "anat.txt"
-
-    questions = []
-    mistakes = set()
-
-
-
-    load_questions(file)
-
-
-    #User specifies start and stop points
-    print("\nВведите номер вопроса, с которого хотите начать (по умолчанию 1)")
-    start = input()
-
-    try:
-        if start == '':
-            start = 0
-        else:
-            start = int(start) - 1
-    except ValueError:
-        print(f"{Fore.RED}Неверный ввод, вы начнете с первого вопроса{Fore.CYAN}")
-        start = 0
-
-
-    print(f"Введите номер вопроса, которым хотите закончить (по умолчанию {len(questions)})")
-    ends = input()
-
-    try:
-        if ends == '':
-            ends = len(questions)
-        elif int(ends) > len(questions) or int(ends) < start:
-            print(f"{Fore.RED}Неверный ввод, вы закончите вопросом номер {len(questions)} {Fore.CYAN}")
-            ends = len(questions)
-        else:
-            ends = int(ends)
-    except ValueError:
-        print(f"{Fore.RED}Неверный ввод, вы закончите вопросом номер {len(questions)} {Fore.CYAN}")
-        ends = len(questions)
-
-    print(f"Вопросы в выбранном Вами диапазоне перемешаны автоматически \
-            \n{Fore.RED}Вводите ответ как на ЕГЭ \
-            \n(цифры в любой последовательности, без дополнительных знаков){Fore.CYAN}\n")
-    chosen = questions[start:ends]
-    shuffle(chosen)
-    for q in chosen:
-        print(f"[{chosen.index(q) + 1}/{len(chosen)}]", end=" ")
-        q.printq()
-        no_mistakes = q.check(input())
-        if not no_mistakes:
-            mistakes.add(q)
-        print("")
-
-    while len(mistakes) != 0:
-
-        print(f"Количество Ваших ошибок: {len(mistakes)}\n"
-            "Сделать работу над ошибками? Введите 1 и нажмите Enter, чтобы выполнить "
-            "\n(Другие цифры или буквы, или просто нажатие Enter выключат программу)")
-        if input() == "1":
-            pending_rem = set()
-            i = 1
-            for q in mistakes:
-                print(f"[{i}/{len(mistakes)}]", end=" ")
-                q.printq()
-                no_mistakes = q.check(input())
-                if no_mistakes:
-                    pending_rem.add(q)
-                i += 1
-                print("")
-            mistakes = mistakes - pending_rem
-        else:
-            mistakes.clear()
+    new_quize = quize()
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-from huelina.huelina import version
+# -*- coding: utf-8 -*-
+
+from huelina.huelina import __version__
 import setuptools
+
 with open ("README.md","r") as fh:
     long_descr = fh.read()
 
-
-
-
 setuptools.setup(
     name='Huelina',
-    version=str(version),
+    version=str(__version__),
     packages=setuptools.find_packages(),
     url='https://github.com/DrNightingales/Huelina',
     license='GNU GPL v3.0',
     author='DrNightingales',
     author_email='petersoloviev123@gmail.com',
     description='A program for solving tests with multiple choice',
-    install_requires="colorama>=0.3.4",
+    install_requires="colorama>=0.3.9",
     python_requires='>=3.6',
     long_description=long_descr,
     long_description_content_type="text/markdown"
