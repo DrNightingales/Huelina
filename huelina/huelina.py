6d59320dfec4fc7bdc678c4e7f472643b7c1770e
DrNightingales <petersoloviev123@gmail.com>
2018-08-16 07:37:49 +0300

    Minor code refactoring

--- huelina/huelina.py
+++ huelina/huelina.py
@@ -4,22 +4,6 @@ version = 0.3
 from huelina.question import Question
 from colorama import Fore, init
 from random import shuffle
-init()
-
-#print(Style.BRIGHT) # Set style 
-print(Fore.CYAN) # Color background text in blue
-
-print(f"Добро пожаловать в Хуелина v{version}\n \
-    \nВведите имя файла с тестами (по умолчанию anat.txt) \
-    \nИспользуйте файлы anatN-ansi.txt на windows и anatN-utf8 на других OS")
-
-file = input()
-
-if file == '':
-    file = "anat.txt"
-
-questions = []
-mistakes = set()
 
 
 def load_questions(filepath):
@@ -37,67 +21,87 @@ def load_questions(filepath):
             else:
                 questions[-1].add_incorrect(line[:len(line) - 1])
 
-load_questions(file)
+
+if __name__ == "__main__":
+    init()
+    #print(Style.BRIGHT) # Set style 
+    print(Fore.CYAN) # Color background text in blue
+
+    print(f"Добро пожаловать в Хуелина v{version}\n \
+        \nВведите имя файла с тестами (по умолчанию anat.txt) \
+        \nИспользуйте файлы anatN-ansi.txt на windows и anatN-utf8 на других OS")
+
+    file = input()
+
+    if file == '':
+        file = "anat.txt"
+
+    questions = []
+    mistakes = set()
 
 
-#User specifies start and stop points
-print("\nВведите номер вопроса, с которого хотите начать (по умолчанию 1)")
-start = input()
 
-try:
-    if start == '':
+    load_questions(file)
+
+
+    #User specifies start and stop points
+    print("\nВведите номер вопроса, с которого хотите начать (по умолчанию 1)")
+    start = input()
+
+    try:
+        if start == '':
+            start = 0
+        else:
+            start = int(start) - 1
+    except ValueError:
+        print(f"{Fore.RED}Неверный ввод, вы начнете с первого вопроса{Fore.CYAN}")
         start = 0
-    else:
-        start = int(start) - 1
-except ValueError:
-    print(f"{Fore.RED}Неверный ввод, вы начнете с первого вопроса{Fore.CYAN}")
-    start = 0
 
 
-print(f"Введите номер вопроса, которым хотите закончить (по умолчанию {len(questions)})")
-ends = input()
+    print(f"Введите номер вопроса, которым хотите закончить (по умолчанию {len(questions)})")
+    ends = input()
 
-try:
-    if ends == '':
-        ends = len(questions)
-    elif int(ends) > len(questions) or int(ends) < start:
+    try:
+        if ends == '':
+            ends = len(questions)
+        elif int(ends) > len(questions) or int(ends) < start:
+            print(f"{Fore.RED}Неверный ввод, вы закончите вопросом номер {len(questions)} {Fore.CYAN}")
+            ends = len(questions)
+        else:
+            ends = int(ends)
+    except ValueError:
         print(f"{Fore.RED}Неверный ввод, вы закончите вопросом номер {len(questions)} {Fore.CYAN}")
         ends = len(questions)
-    else:
-        ends = int(ends)
-except ValueError:
-    print(f"{Fore.RED}Неверный ввод, вы закончите вопросом номер {len(questions)} {Fore.CYAN}")
-    ends = len(questions)
 
-print(f"Вопросы в выбранном Вами диапазоне перемешаны автоматически \
-        \n{Fore.RED}Вводите ответ как на ЕГЭ \
-        \n(цифры в любой последовательности, без дополнительных знаков){Fore.CYAN}\n")
-chosen = questions[start:ends]
-shuffle(chosen)
-for q in chosen:
-    print(f"[{chosen.index(q) + 1}/{len(chosen)}]", end=" ")
-    q.printq()
-    no_mistakes = q.check(input())
-    if not no_mistakes:
-        mistakes.add(q)
-    print("")
+    print(f"Вопросы в выбранном Вами диапазоне перемешаны автоматически \
+            \n{Fore.RED}Вводите ответ как на ЕГЭ \
+            \n(цифры в любой последовательности, без дополнительных знаков){Fore.CYAN}\n")
+    chosen = questions[start:ends]
+    shuffle(chosen)
+    for q in chosen:
+        print(f"[{chosen.index(q) + 1}/{len(chosen)}]", end=" ")
+        q.printq()
+        no_mistakes = q.check(input())
+        if not no_mistakes:
+            mistakes.add(q)
+        print("")
 
-while len(mistakes) != 0:
+    while len(mistakes) != 0:
 
-    print(f"Количество Ваших ошибок: {len(mistakes)}\n"
-          "Сделать работу над ошибками? Введите 1 и нажмите Enter, чтобы выполнить "
-          "\n(Другие цифры или буквы, или просто нажатие Enter выключат программу)")
-    if input() == "1":
-        pending_rem = set()
-        i = 1
-        for q in mistakes:
-            print(f"[{i}/{len(mistakes)}]", end=" ")
-            q.printq()
-            no_mistakes = q.check(input())
-            if no_mistakes:
-                pending_rem.add(q)
-            i += 1
-            print("")
-        mistakes = mistakes - pending_rem
-    else:
-        mistakes.clear()
+        print(f"Количество Ваших ошибок: {len(mistakes)}\n"
+            "Сделать работу над ошибками? Введите 1 и нажмите Enter, чтобы выполнить "
+            "\n(Другие цифры или буквы, или просто нажатие Enter выключат программу)")
+        if input() == "1":
+            pending_rem = set()
+            i = 1
+            for q in mistakes:
+                print(f"[{i}/{len(mistakes)}]", end=" ")
+                q.printq()
+                no_mistakes = q.check(input())
+                if no_mistakes:
+                    pending_rem.add(q)
+                i += 1
+                print("")
+            mistakes = mistakes - pending_rem
+        else:
+            mistakes.clear()
--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from huelina.huelina import version
+import setuptools
+with open ("README.md","r") as fh:
+    long_descr = fh.read()
 
-try:
-    from setuptools import setup
-except:
-    from distutils.core import setup
 
-setup(
+
+
+setuptools.setup(
     name='Huelina',
     version=str(version),
-    packages=['huelina'],
+    packages=setuptools.find_packages(),
     url='https://github.com/DrNightingales/Huelina',
     license='GNU GPL v3.0',
     author='DrNightingales',
     author_email='petersoloviev123@gmail.com',
     description='A program for solving tests with multiple choice',
-    install_requires="colorama",
-    python_requires='>=3.6'
+    install_requires="colorama>=0.3.4",
+    python_requires='>=3.6',
+    long_description=long_descr,
+    long_description_content_type="text/markdown"
 )
